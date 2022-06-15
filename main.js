d9ee6a647a97633801a811d1c7ad79fe20b2b013
Anna Cho <loveablessing@gmail.com>
2022-06-14 20:37:23 -0400

    commit tree b398dbab61fd067ef4eccf39b2732090929f69ed

--- main.js
+++ main.js
@@ -4,6 +4,30 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let classHearts = document.querySelectorAll(".like-glyph")
+
+function like(event){
+  //get the element that triggered the event
+  let heart = event.target
+  //invoke mimicServerCall to simulate making a server request
+  mimicServerCall(url="file://///wsl$/Ubuntu/home/chewoo/Development/code/phase-1-building-simple-liker/index.html")
+  .then(function(){
+    //if EMPTY_HEART, change to FULL_HEART AND CHANGE COLOR BY CLASS, else if FULL_HEART, change to EMPTY_HEART and change class if any to remove color
+    if (heart.innerText === EMPTY_HEART){
+      heart.innerText = FULL_HEART
+      heart.className = "activated-heart"
+    } else {
+      heart.innerText = EMPTY_HEART
+      heart.className = ""
+    }
+  })
+}
+
+for (const state of classHearts) {
+  state.addEventListener("click", like)
+}
+
+
 
 
 
